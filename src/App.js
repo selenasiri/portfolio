8ae173f52ceac9d1f60d4b03d7e5002e4c1260ee
Ying Lu <yinglu91@gmail.com>
2020-01-04 20:28:29 -0500

    added bio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio }); // ok
+    // this.setState(prev => ({ displayBio: !prev.displayBio })); // ok, more safe
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Selena.</p>
+        <p>I am always looking forward to working on meaningful projects.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in America and code almost every day.</p>
+            <p>My favorite languages are JavaScript and React.</p>
+            <p>Besides coding, I also love piano and cooking!</p>
+            <button onClick={this.toggleDisplayBio}>Show less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;

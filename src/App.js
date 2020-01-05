42d2eb61598eb88b8bf688e3b968b0a9a8ac4774
Ying Lu <yinglu91@gmail.com>
2020-01-05 15:02:12 -0500

    added class components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Projects from './Projects';
 
 class App extends Component {
   state = { displayBio: false };
@@ -26,6 +27,8 @@ class App extends Component {
             <button onClick={this.toggleDisplayBio}>Read more</button>
           </div>
         )}
+        <hr />
+        <Projects />
       </div>
     );
   }
